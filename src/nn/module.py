276971bcb3ee697243466e8e3158ba2d75c32c56
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-19 14:59:01 +0200

    Add base Module class for neural network layers

--- src/nn/module.py
+++ src/nn/module.py
@@ -1,17 +1,49 @@
+from core.parameter import Parameter
+
+
 class Module:
-    def __init__(self):
-        self._parameters = {}
-        self._modules = {}
-
-    def forward(self, x):
-        raise NotImplementedError
-
-    def __call__(self, x):
-        return self.forward(x)
+    """
+    Base class for all neural network modules (layers, models, etc.)
+    """
 
     def parameters(self):
-        return self._parameters.values()
+        """
+        Return all Parameter objects inside this module.
+        This includes parameters of submodules.
+        """
+        params = []
 
-    def add_module(self, name, module):
-        self._modules[name] = module
+        for attr_name in dir(self):
+            attr = getattr(self, attr_name)
+
+            # If it's a Parameter, collect it
+            if isinstance(attr, Parameter):
+                params.append(attr)
+
+            # If it's a Module, collect its parameters too
+            if isinstance(attr, Module):
+                params.extend(attr.parameters())
+
+        return params
+
+    def zero_grad(self):
+        """
+        Reset gradients of all parameters.
+        """
+        for p in self.parameters():
+            p.zero_grad()
+
+    def __call__(self, *args, **kwargs):
+        """
+        Allows the module to be called like a function.
+        Example: output = layer(input)
+        """
+        return self.forward(*args, **kwargs)
+
+    def forward(self, *args, **kwargs):
+        """
+        Must be implemented by subclasses.
+        """
+        raise NotImplementedError("Forward method not implemented")
+
 
