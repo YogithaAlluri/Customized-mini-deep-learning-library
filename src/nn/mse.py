9139707ab96324db871463a40e0f47280f27387d
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-19 17:07:32 +0200

    Fix mse

--- src/nn/mse.py
+++ src/nn/mse.py
@@ -4,26 +4,23 @@ from core.tensor import Tensor
 
 class MSELoss(Module):
     def forward(self, y_pred, y_true):
-        # Tensor operations (keeps graph)
-        diff = y_pred - y_true          # (N,1)
-        sq = diff * diff                # (N,1)
+        diff = y_pred - y_true
+        sq = diff * diff
 
-        # Mean factor (scalar)
+        # Mean factor
         mean = Tensor(1.0 / sq.data.size, requires_grad=False)
-
-        # Elementwise mean
-        reduced = sq * mean             # (N,1)
+        reduced = sq * mean  # (N,1)
 
         # ---- Reduce to scalar WITHOUT breaking graph ----
-        # We manually sum elements using Tensor addition
+        # We multiply each element by a Tensor(1) to keep graph
         total = None
-        flat = reduced.data.flatten()
-
-        for i in range(len(flat)):
-            elem = Tensor(flat[i], requires_grad=True)
+        for i in range(reduced.data.shape[0]):
+            elem_value = reduced.data[i, 0]
+            elem = Tensor(elem_value, requires_grad=True)
             total = elem if total is None else total + elem
 
         return total
+
 
 
 
