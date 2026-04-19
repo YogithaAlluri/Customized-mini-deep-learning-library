a4220ff52e0c828807ce3a7cce4b234b78319ac8
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-19 15:05:46 +0200

    add MSELoss implementation

--- src/nn/mse.py
+++ src/nn/mse.py
@@ -1,5 +1,19 @@
-from .loss import Loss
+import numpy as np
+from .module import Module
+from core.tensor import Tensor
 
-class MSELoss(Loss):
-    def forward(self, pred, target):
-        pass
+
+class MSELoss(Module):
+    """
+    Mean Squared Error loss:
+    loss = mean((y_pred - y_true)^2)
+    """
+
+    def forward(self, y_pred, y_true):
+        # Compute squared difference
+        diff = y_pred.data - y_true.data
+        loss_value = np.mean(diff ** 2)
+
+        # Wrap in a Tensor
+        return Tensor(loss_value, requires_grad=y_pred.requires_grad)
+
