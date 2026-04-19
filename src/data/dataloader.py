637c4bb74b07ef71c870151662fc269249b63db4
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-19 15:13:21 +0200

    add datset and dataloader classes

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,11 +1,32 @@
+import numpy as np
+
+
 class DataLoader:
-    def __init__(self, dataset, batch_size=1, shuffle=False):
+    """
+    Simple DataLoader that returns batches from a Dataset.
+    """
+
+    def __init__(self, dataset, batch_size=1, shuffle=True):
         self.dataset = dataset
         self.batch_size = batch_size
         self.shuffle = shuffle
 
     def __iter__(self):
-        return self
+        # Create an index list
+        indices = np.arange(len(self.dataset))
 
-    def __next__(self):
-        pass
+        # Shuffle if needed
+        if self.shuffle:
+            np.random.shuffle(indices)
+
+        # Yield batches
+        for start in range(0, len(indices), self.batch_size):
+            batch_idx = indices[start:start + self.batch_size]
+
+            batch = [self.dataset[i] for i in batch_idx]
+
+            # Unzip batch into x and y
+            xs, ys = zip(*batch)
+
+            yield xs, ys
+
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,11 @@
 class Dataset:
+    """
+    Base class for datasets.
+    """
+
     def __len__(self):
-        raise NotImplementedError
+        raise NotImplementedError("Dataset must implement __len__")
 
     def __getitem__(self, idx):
-        raise NotImplementedError
+        raise NotImplementedError("Dataset must implement __getitem__")
+
