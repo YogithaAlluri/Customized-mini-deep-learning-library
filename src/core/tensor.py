1ba7cd9e59721ddf33abf7505be884888057056f
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-19 15:57:30 +0200

    fix indenation in tensor

--- src/core/tensor.py
+++ src/core/tensor.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 
-
-
-
-
 class Tensor:
     def __init__(self, data, requires_grad=False):
         """
@@ -23,89 +19,76 @@ class Tensor:
         # This will later point to a GradFunction that created this tensor
         self.grad_fn = None
 
-    def backward(self):
-        """
-        Entry point for backpropagation.
-
-        For now, this is just a placeholder.
-        Later, this will:
-        - check that this is a scalar
-        - initialize grad to 1.0
-        - walk back through grad_fn to compute gradients.
-        """
-        if not self.requires_grad:
-            return
-        # Implementation will come later
-        pass
+        # Parents in the computation graph
+        self.parents = []
 
     def zero_grad(self):
-        """
-        Reset the gradient to None.
-        Called before each new optimization step.
-        """
+        """Reset the gradient to None."""
         self.grad = None
 
     def detach(self):
-        """
-        Return a new Tensor with the same data but no gradient tracking.
-        Useful when you want to break the computation graph.
-        """
+        """Return a new Tensor with same data but no gradient tracking."""
         return Tensor(self.data, requires_grad=False)
+
     def set_grad_fn(self, grad_fn, parents):
+        """Attach grad_fn and parent tensors."""
         self.grad_fn = grad_fn
         self.parents = parents
-    
+
+    # -----------------------------
+    # AUTOGRAD BACKWARD IMPLEMENTATION
+    # -----------------------------
+    def backward(self, grad_output=None):
+        """
+        Compute gradients for all tensors in the computation graph.
+        """
+
+        # If this is the final scalar, gradient = 1
+        if grad_output is None:
+            grad_output = 1.0
+
+        # Initialize gradient for this tensor
+        self.grad = grad_output
+
+        # Stack for graph traversal
+        stack = [self]
+
+        while stack:
+            t = stack.pop()
+
+            # If no grad_fn, nothing to backpropagate
+            if t.grad_fn is None:
+                continue
+
+            # Call backward of the operation
+            grads = t.grad_fn.backward(t.grad)
+
+            # grads is a tuple: (grad_x, grad_y, ...)
+            for parent, grad in zip(t.parents, grads):
+
+                # Accumulate gradient
+                if parent.grad is None:
+                    parent.grad = grad
+                else:
+                    parent.grad += grad
+
+                # Continue backprop if parent has a grad_fn
+                if parent.grad_fn is not None:
+                    stack.append(parent)
+
+    # -----------------------------
+    # OPERATORS
+    # -----------------------------
     def __add__(self, other):
         from core.ops.add import add
         return add(self, other)
 
     def __mul__(self, other):
-       from core.ops.mul import mul
-       return mul(self, other)
+        from core.ops.mul import mul
+        return mul(self, other)
 
     def __matmul__(self, other):
-       from core.ops.matmul import matmul
-       return matmul(self, other)
+        from core.ops.matmul import matmul
+        return matmul(self, other)
 
-    def backward(self, grad_output=None):
-        """
-    Compute gradients for all tensors in the computation graph.
-    """
-
-    # If this is the final scalar, gradient = 1
-    if grad_output is None:
-        grad_output = 1.0
-
-    # Initialize gradient for this tensor
-    self.grad = grad_output
-
-    # Stack for graph traversal
-    stack = [self]
-
-    while stack:
-        t = stack.pop()
-
-        # If no grad_fn, nothing to backpropagate
-        if t.grad_fn is None:
-            continue
-
-        # Call backward of the operation
-        grads = t.grad_fn.backward(t.grad)
-
-        # grads is a tuple: (grad_x, grad_y, ...)
-        for parent, grad in zip(t.parents, grads):
-
-            # Accumulate gradient
-            if parent.grad is None:
-                parent.grad = grad
-            else:
-                parent.grad += grad
-
-            # Continue backprop if parent has a grad_fn
-            if parent.grad_fn is not None:
-                stack.append(parent)
-
-
-
-
-
+ 
