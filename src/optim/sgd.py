1746afa407065b8843ea1e240caf4c62d48cb916
YogithaAlluri <yogitha.allurii@gmail.com>
2026-04-20 22:16:14 +0200

    fix sgd

--- src/optim/sgd.py
+++ src/optim/sgd.py
@@ -1,27 +1,10 @@
 class SGD:
-    """
-    Stochastic Gradient Descent optimizer.
-    Updates parameters using: param -= lr * param.grad
-    """
-
     def __init__(self, parameters, lr=0.01):
         self.parameters = parameters
         self.lr = lr
-
-    def step(self):
-        """
-        Update all parameters.
-        """
-        for p in self.parameters:
-            if p.grad is None:
-                continue
-            # Gradient descent update
-            p.data = p.data - self.lr * p.grad
-
     def zero_grad(self):
-        """
-        Reset gradients of all parameters.
-        """
+        for p in self.parameters: p.zero_grad()
+    def step(self):
         for p in self.parameters:
-            p.zero_grad()
-
+            if p.grad is not None:
+                p.data -= self.lr * p.grad
