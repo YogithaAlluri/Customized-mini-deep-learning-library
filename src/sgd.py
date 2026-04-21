4aec9bd504d83295cf208c1020207a1cac3dd683
shadifaraji <136476348+shadifaraji@users.noreply.github.com>
2026-04-21 10:07:52 +0200

    Add files via upload

---
+++ src/sgd.py
@@ -0,0 +1,27 @@
+class SGD:
+    """
+    Stochastic Gradient Descent optimizer.
+    Updates parameters using: param -= lr * param.grad
+    """
+
+    def __init__(self, parameters, lr=0.01):
+        self.parameters = parameters
+        self.lr = lr
+
+    def step(self):
+        """
+        Update all parameters.
+        """
+        for p in self.parameters:
+            if p.grad is None:
+                continue
+            # Gradient descent update
+            p.data = p.data - self.lr * p.grad
+
+    def zero_grad(self):
+        """
+        Reset gradients of all parameters.
+        """
+        for p in self.parameters:
+            p.zero_grad()
+
